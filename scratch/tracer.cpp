37afe1eb250c35cb1e4a4cf1c2c85c9d6fb6657a
Lu Verissimo <66143267+LuVerissimo@users.noreply.github.com>
2026-07-26 15:21:31 -0400

    Experiment 1&2 two Tracers in nested scopes pass by value vs by reference

---
+++ scratch/tracer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+struct Tracer {
+    std::string name;
+
+    Tracer(std::string n) : name(std::move(n)) {
+        std::cout << "[CTOR] " << name << "\n";
+    }
+
+    Tracer(const Tracer& other) : name(other.name + " (copy)") {
+        std::cout << "[COPY] " << name << "\n";
+    }
+
+    Tracer& operator=(const Tracer& other) {
+        name = other.name;
+        std::cout << "[COPY=] " << name << "\n";
+        return *this;
+    }
+
+    ~Tracer() {
+        std::cout << "[DTOR] " << name << "\n";
+    }
+};
+
+void by_value(Tracer t) {
+    std::cout << "  inside by_value\n";
+}
+
+void by_ref(const Tracer& t) {
+    std::cout << "  inside by_ref\n";
+}
+
+int main() {
+    Tracer a("A");
+    std::cout << "--- by value ---\n";
+    by_value(a);
+    std::cout << "--- by ref ---\n";
+    by_ref(a);
+}
