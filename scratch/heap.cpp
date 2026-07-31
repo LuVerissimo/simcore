19402d02b2c145b0d175900ea57c0feaa049bd7d
Lu Verissimo <66143267+LuVerissimo@users.noreply.github.com>
2026-07-30 22:50:42 -0400

    move CTO and move assignment

--- scratch/heap.cpp
+++ scratch/heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class HeapArray {
     double* data_;
@@ -23,7 +24,30 @@ public:
         for (int i = 0; i < size_; ++i) {
             data_[i] = other.data_[i];
         }
-    }   
+    }
+    
+    // move CTO
+    HeapArray(HeapArray&& other) noexcept : data_(nullptr), size_(0) {
+
+        data_ = other.data_;
+        size_ = other.size_;
+
+        other.data_ = nullptr;
+        other.size_ = 0;
+    }
+    HeapArray& operator=(HeapArray&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+
+            data_ = other.data_;
+            size_ = other.size_;
+
+            other.data_ = nullptr;
+            other.size_ = 0;
+        }
+
+        return *this;
+    }
 
     HeapArray& operator=(const HeapArray& other) {
         if (this == &other) {
@@ -52,29 +76,32 @@ public:
     }
 };
 
-
 int main() {
-    // HeapArray arr(5);
 
-    // arr[0] = 3.14;
-    // std::cout << "Element 0: " << arr[0] << std::endl;
-    // std::cout << "Array size: " << arr.size() << std::endl;
+};
 
-    // return 0;
+// int main() {
+//     // HeapArray arr(5);
 
-    // Double Free
-    // HeapArray a(5);
-    // a[0] = 3.14;
-    // HeapArray b = a;   // shallow copy — b.data_ == a.data_
-    // b[0] = 999;
-    // std::cout << a[0] << "\n";  // 999 — your original got mutated
+//     // arr[0] = 3.14;
+//     // std::cout << "Element 0: " << arr[0] << std::endl;
+//     // std::cout << "Array size: " << arr.size() << std::endl;
 
-    //use-after-free
-    double* stolen;
-    {
-        HeapArray a(5);
-        a[0] = 3.14;
-        stolen = &a[0];
-    }   // a destroyed here
-    std::cout << *stolen << "\n";   // dangling pointer
-}
+//     // return 0;
+
+//     // Double Free
+//     // HeapArray a(5);
+//     // a[0] = 3.14;
+//     // HeapArray b = a;   // shallow copy — b.data_ == a.data_
+//     // b[0] = 999;
+//     // std::cout << a[0] << "\n";  // 999 — your original got mutated
+
+//     //use-after-free
+//     double* stolen;
+//     {
+//         HeapArray a(5);
+//         a[0] = 3.14;
+//         stolen = &a[0];
+//     }   // a destroyed here
+//     std::cout << *stolen << "\n";   // dangling pointer
+// }
