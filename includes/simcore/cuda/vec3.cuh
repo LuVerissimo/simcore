3fa962aa40190ab6f61d068420abb49561065806
Lu Verissimo <your-github-email@example.com>
2026-09-14 22:28:17 -0400

    removed const from pointer C for Kernel

--- includes/simcore/cuda/vec3.cuh
+++ includes/simcore/cuda/vec3.cuh
@@ -4,7 +4,7 @@
 #include <string>
 
 __global__
-void vecAddKernel(const vec3f* A, const vec3f* B, const vec3f* C, int n) {
+void vecAddKernel(const vec3f* A, const vec3f* B, vec3f* C, int n) {
     int i = threadIdx.x + blockDim.x * blockIdx.x;
     if (i < n) {
         C[i] = A[i] + B[i];
